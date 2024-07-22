144e811e832a031dac6172c80e1e2d7fd10fb0a9
Areefshaik2002 <Areefshaik2002@gmail.com>
2024-07-22 10:52:08 +0530

    first commit

--- src/oops/Banking2.java
+++
@@ -1,7 +0,0 @@
-package oops;
-
-public class Banking2 {
- public static void main(String[] args) {
-	
-}
-}
---
+++ src/oops/Final.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class Final {
+	
+/*	 if we use a final keyword before creating a variable,
+then the value for that variable cannot be reassigned.*/
+	
+		final static int j = 56;
+		
+	public static void main(String[] args) {
+		
+			System.out.println(j);
+
+	}
+
+}
