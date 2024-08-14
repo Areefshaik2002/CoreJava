07bbe4c9f40747e2e71a899b91ab5b1311baf797
Areefshaik2002 <Areefshaik2002@gmail.com>
2024-08-14 10:36:07 +0530

    21st commit

--- src/threads/ThreadsUsingInterface.java
+++ src/threads/ThreadsUsingInterface.java
@@ -4,46 +4,43 @@ class T1 extends Thread{
 	public void run() {
 		for(int i=1;i<=10;i++) {
 			System.out.println(i+" "+Thread.currentThread().getName());
+			try {
+				Thread.sleep(100);
+			}catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+			
 		}
 	}
 }
 public class ThreadsUsingInterface {
 	public static void main(String[] args) {
-	T1 obj  = new T1();
-	Thread t1 = new Thread(obj);
-	Thread t2 = new Thread(obj);
-	Thread t3 = new Thread(obj);
+	T1 obj1  = new T1();
+	T1 obj2 = new T1();
+	T1 obj3 = new T1();
+	
+ 	Thread t1 = new Thread(obj1);
+	Thread t2 = new Thread(obj2);
+	Thread t3 = new Thread(obj3);
 	t1.setName("1st Thread");
-	t1.setPriority(9);
+	t1.setPriority(1);
 	t1.start();
-	try {
-		t1.join();
-		t1.sleep(5000);
-	} catch (InterruptedException e) {
-		e.printStackTrace();
-	}
 	
-//	Thread t2 = new Thread(obj);
 	t2.setName("2nd thread");
-	t2.setPriority(8);
+	t2.setPriority(2);
 	t2.start();
-	try {
-		t2.join();
-		t2.sleep(5000);
-	} catch (InterruptedException e) {
-		e.printStackTrace();
-	}
 	
-//	Thread t3 = new Thread(obj);
-	t3.setName("3nd thread");
-	t3.setPriority(7);
+	t3.setName("3rd thread");
+	t3.setPriority(3);
 	t3.start();
 	try {
+		t1.join();
+		t2.join();
 		t3.join();
-		t3.sleep(5000);
 	} catch (InterruptedException e) {
 		e.printStackTrace();
 	}
+	System.out.println("finished");
 	
 	
 }
